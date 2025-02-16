8f82703cde26e2d970d2f8480794e61a1bc7930b
farah-nissa <91769087+farah-nissa@users.noreply.github.com>
2025-02-17 00:04:25 +0700

    Update income_by_year.py

--- income_by_year.py
+++ income_by_year.py
@@ -1,56 +1,12 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 st.title("Income by Year")
-2025-02-16 20:24:16.386 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-2025-02-16 20:24:17.356 WARNING streamlit: 
-  [33m[1mWarning:[0m to view a Streamlit app on a browser, use Streamlit in a file and
-  run it with the following command:
 
-    streamlit run [FILE_NAME] [ARGUMENTS]
-2025-02-16 20:24:17.396 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-DeltaGenerator()
 data = pd.read_csv('C:\\\\Coding\\\\income_by_year.csv')
                      
 filter_value = st.sidebar.slider("Filter Data by Value", min_value=0, max_value=100)
                      
-2025-02-16 20:25:41.068 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-2025-02-16 20:25:41.130 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-2025-02-16 20:25:41.139 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-2025-02-16 20:25:41.148 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-2025-02-16 20:25:41.156 WARNING streamlit.runtime.state.session_state_proxy: Session state does not function when running a script without `streamlit run`
-2025-02-16 20:25:41.164 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-2025-02-16 20:25:41.178 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
 filtered_data = data[data['income'] > filter_value]
                      
->>> st.bar_chart(filtered_data['value'])
-...                      
-Traceback (most recent call last):
-  File "C:\Users\ASUS\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\site-packages\pandas\core\indexes\base.py", line 3805, in get_loc
-    return self._engine.get_loc(casted_key)
-  File "index.pyx", line 167, in pandas._libs.index.IndexEngine.get_loc
-  File "index.pyx", line 196, in pandas._libs.index.IndexEngine.get_loc
-  File "pandas\\_libs\\hashtable_class_helper.pxi", line 7081, in pandas._libs.hashtable.PyObjectHashTable.get_item
-  File "pandas\\_libs\\hashtable_class_helper.pxi", line 7089, in pandas._libs.hashtable.PyObjectHashTable.get_item
-KeyError: 'value'
-
-The above exception was the direct cause of the following exception:
-
-Traceback (most recent call last):
-  File "<pyshell#7>", line 1, in <module>
-    st.bar_chart(filtered_data['value'])
-  File "C:\Users\ASUS\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\site-packages\pandas\core\frame.py", line 4102, in __getitem__
-    indexer = self.columns.get_loc(key)
-  File "C:\Users\ASUS\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\site-packages\pandas\core\indexes\base.py", line 3812, in get_loc
-    raise KeyError(key) from err
-KeyError: 'value'
->>> st.bar_chart(filtered_data['income])
-...                            
-SyntaxError: unterminated string literal (detected at line 1)
->>> st.bar_chart(filtered_data['income'])
-...                            
-2025-02-16 20:27:02.769 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-2025-02-16 20:27:02.790 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
-DeltaGenerator()
+st.bar_chart(filtered_data['income'])
