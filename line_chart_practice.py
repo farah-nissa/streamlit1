3c0d55b77f5ca98b4d043c2563b66c48459863a0
farah-nissa <91769087+farah-nissa@users.noreply.github.com>
2025-02-17 01:07:16 +0700

    Update line_chart_practice.py

--- line_chart_practice.py
+++ line_chart_practice.py
@@ -8,23 +8,4 @@ data = pd.DataFrame({
     "x": [1, 2, 3, 4],
     "y": [10, 20, 30, 40]})
 
-plt.plot(df["x"], df["y"])
-
-st.pyplot()
-
-num_points = st.slider("Number of points", min_value=100, max_value=1000, value=500, step=100)
-
-text_input = st.text_input("Enter some text: ")
-
-st.write("The name: ", text_input)
-
-show_plot = st.checkbox("Show plot", value=True)
-
-if not show_plot:
-    plt.close()
-
-plot_color = st.selectbox("Plot color", ["blue", "red", "green"])
-
-plt.plot(data["x"], data["y"], color=plot_color)
-
-st.camera_input("Take a picture")
+df.plot(x="x", y="y", kind='plot')
