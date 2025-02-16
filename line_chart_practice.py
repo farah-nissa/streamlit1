2b32105d025100161b319e1d24632936a7d6f1d0
farah-nissa <91769087+farah-nissa@users.noreply.github.com>
2025-02-17 02:03:27 +0700

    Update line_chart_practice.py

--- line_chart_practice.py
+++ line_chart_practice.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 st.title("Line Plot Practice")
 
 fig, df = plt.subplots()
-df.plot = ([5, 10, 15, 20], [10, 20, 30, 40])
+df.bar = ([5, 10, 15, 20], [10, 20, 30, 40])
 
 plt.xlim(0, 40)
 plt.ylim(0,40)
